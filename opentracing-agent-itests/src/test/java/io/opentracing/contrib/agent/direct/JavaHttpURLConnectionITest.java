a1d72df1951cd4555e1af632a5eb6fa86ca98067
Gary Brown <gary@brownuk.com>
2017-02-20 15:06:49 +0000

    Update httpurlconnection tests to use okhttp3 mock server

--- opentracing-agent-itests/src/test/java/io/opentracing/contrib/agent/direct/JavaHttpURLConnectionITest.java
+++ opentracing-agent-itests/src/test/java/io/opentracing/contrib/agent/direct/JavaHttpURLConnectionITest.java
@@ -24,154 +24,153 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import io.opentracing.contrib.agent.common.OTAgentTestBase;
 import io.opentracing.mock.MockSpan;
 import io.opentracing.tag.Tags;
+import okhttp3.HttpUrl;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 
 public class JavaHttpURLConnectionITest extends OTAgentTestBase {
 
-    private static final String SAY_HELLO_URL = "http://localhost:8180/hello";
-
-    private static Server server = null;
-
-    public static final String TEST_FAULT_HEADER_FLAG = "test-fault";
-
-    @BeforeClass
-    public static void initClass() throws Exception {
-        server = new Server(8180);
-        server.setHandler(new HelloHandler());
-        server.start();
-    }
-
-    @AfterClass
-    public static void closeClass() throws Exception {
-        server.stop();
-    }
-
     @Test
     public void testHttpURLConnectionConnectGET() throws IOException {
-        testHttpRequestConnect(new URL(SAY_HELLO_URL), "GET", false);
+        testHttpRequestConnect("GET", false);
     }
 
     @Test
     public void testHttpURLConnectionGetStatusCodeGET() throws IOException {
-        testHttpRequestGetStatusCode(new URL(SAY_HELLO_URL), "GET", false);
+        testHttpRequestGetStatusCode("GET", false);
     }
 
     @Test
     public void testHttpURLConnectionInputStreamGET() throws IOException {
-        sendRequestUsingInputStream(new URL(SAY_HELLO_URL), "GET", false);
+        sendRequestUsingInputStream("GET", false);
     }
 
     @Test
     public void testHttpURLConnectionConnectGETWithFault() throws IOException {
-        testHttpRequestConnect(new URL(SAY_HELLO_URL), "GET", false);
+        testHttpRequestConnect("GET", true);
     }
 
     @Test
     public void testHttpURLConnectionInputStreamGETWithFault() throws IOException {
-        sendRequestUsingInputStream(new URL(SAY_HELLO_URL), "GET", true);
+        sendRequestUsingInputStream("GET", true);
     }
 
     @Test
     public void testHttpURLConnectionConnectPUT() throws IOException {
-        testHttpRequestConnect(new URL(SAY_HELLO_URL), "PUT", false);
+        testHttpRequestConnect("PUT", false);
     }
 
     @Test
     public void testHttpURLConnectionInputStreamPUT() throws IOException {
-        sendRequestUsingInputStream(new URL(SAY_HELLO_URL), "PUT", false);
+        sendRequestUsingInputStream("PUT", false);
     }
 
     @Test
     public void testHttpURLConnectionConnectPOST() throws IOException {
-        testHttpRequestConnect(new URL(SAY_HELLO_URL), "POST", false);
+        testHttpRequestConnect("POST", false);
     }
 
     @Test
     public void testHttpURLConnectionInputStreamPOST() throws IOException {
-        sendRequestUsingInputStream(new URL(SAY_HELLO_URL), "POST", false);
+        sendRequestUsingInputStream("POST", false);
     }
 
-    protected void testHttpRequestConnect(URL url, String method, boolean fault) throws IOException {
-        sendRequest(url, method, fault, true);
+    protected void testHttpRequestConnect(String method, boolean fault) throws IOException {
+        sendRequest(method, fault, true);
     }
 
-    protected void testHttpRequestGetStatusCode(URL url, String method, boolean fault) throws IOException {
-        sendRequest(url, method, fault, false);
+    protected void testHttpRequestGetStatusCode(String method, boolean fault) throws IOException {
+        sendRequest(method, fault, false);
     }
 
-    protected void sendRequest(URL url, String method, boolean fault, boolean connect) throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    protected void sendRequest(String method, boolean fault, boolean connect) throws IOException {
+        MockWebServer server = new MockWebServer();
 
-        connection.setRequestMethod(method);
-        connection.setDoOutput(true);
-        connection.setUseCaches(false);
-        connection.setAllowUserInteraction(false);
-
-        if (fault) {
-            connection.addRequestProperty(TEST_FAULT_HEADER_FLAG, "true");
-        }
-
-        if (connect) {
-            connection.connect();
-        }
-
-        int status = connection.getResponseCode();
-
-        if (!fault) {
-            assertEquals("Unexpected response code", 200, status);
-        } else {
-            assertEquals("Unexpected fault response code", 401, status);
-        }
-
-        // Call again to make sure does not attempt to finish the span again
-        connection.getResponseCode();
-
-        verifyTrace(url, method, fault);
-    }
-
-    protected void sendRequestUsingInputStream(URL url, String method, boolean fault) throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-        connection.setRequestMethod(method);
-        connection.setDoOutput(true);
-        connection.setUseCaches(false);
-        connection.setAllowUserInteraction(false);
-
-        if (fault) {
-            connection.addRequestProperty(TEST_FAULT_HEADER_FLAG, "true");
-        }
-
-        try (InputStream is = connection.getInputStream()) {
-        } catch (IOException ioe) {
-            if (!fault) {
-                throw ioe;
+        try {
+            if (fault) {
+                server.enqueue(new MockResponse().setResponseCode(401));
+            } else {
+                server.enqueue(new MockResponse().setBody("hello, world!").setResponseCode(200));
             }
+
+            HttpUrl httpUrl = server.url("/hello");
+
+            HttpURLConnection connection = (HttpURLConnection) httpUrl.url().openConnection();
+
+            connection.setRequestMethod(method);
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+            connection.setAllowUserInteraction(false);
+
+            if (connect) {
+                connection.connect();
+            }
+
+            int status = connection.getResponseCode();
+
+            if (!fault) {
+                assertEquals("Unexpected response code", 200, status);
+            } else {
+                assertEquals("Unexpected fault response code", 401, status);
+            }
+
+            // Call again to make sure does not attempt to finish the span again
+            connection.getResponseCode();
+
+            verifyTrace(httpUrl.url(), method, fault);
+        } finally {
+            server.shutdown();
+            server.close();
         }
+    }
 
-        int status = connection.getResponseCode();
+    protected void sendRequestUsingInputStream(String method, boolean fault) throws IOException {
+        MockWebServer server = new MockWebServer();
 
-        if (!fault) {
-            assertEquals("Unexpected response code", 200, status);
-        } else {
-            assertEquals("Unexpected fault response code", 401, status);
+        try {
+            if (fault) {
+                server.enqueue(new MockResponse().setResponseCode(401));
+            } else {
+                server.enqueue(new MockResponse().setBody("hello, world!").setResponseCode(200));
+            }
+
+            HttpUrl httpUrl = server.url("/hello");
+
+            HttpURLConnection connection = (HttpURLConnection) httpUrl.url().openConnection();
+
+            connection.setRequestMethod(method);
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+            connection.setAllowUserInteraction(false);
+
+            try (InputStream is = connection.getInputStream()) {
+            } catch (IOException ioe) {
+                if (!fault) {
+                    throw ioe;
+                }
+            }
+
+            int status = connection.getResponseCode();
+
+            if (!fault) {
+                assertEquals("Unexpected response code", 200, status);
+            } else {
+                assertEquals("Unexpected fault response code", 401, status);
+            }
+
+            // Call again to make sure does not attempt to finish the span again
+            connection.getResponseCode();
+
+            verifyTrace(httpUrl.url(), method, fault);
+        } finally {
+            server.shutdown();
+            server.close();
         }
-
-        // Call again to make sure does not attempt to finish the span again
-        connection.getResponseCode();
-
-        verifyTrace(url, method, fault);
     }
 
     protected void verifyTrace(URL url, String method, boolean fault) {
@@ -180,22 +179,6 @@ public class JavaHttpURLConnectionITest extends OTAgentTestBase {
         assertEquals(method, spans.get(0).operationName());
         assertEquals(url.toString(), spans.get(0).tags().get(Tags.HTTP_URL.getKey()));
         assertEquals(fault ? 401 : 200, spans.get(0).tags().get(Tags.HTTP_STATUS.getKey()));
-    }
-
-    public static class HelloHandler extends AbstractHandler {
-        
-        @Override
-        public void handle(String target, org.eclipse.jetty.server.Request baseRequest, HttpServletRequest request,
-                HttpServletResponse response) throws IOException, ServletException {
-            response.setContentType("text/html;charset=utf-8");
-            if (request.getHeader(TEST_FAULT_HEADER_FLAG) != null) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            } else {
-                response.setStatus(HttpServletResponse.SC_OK);
-                response.getWriter().println("<h1>Hello World</h1>");                
-            }
-            baseRequest.setHandled(true);
-        }
     }
 
 }
