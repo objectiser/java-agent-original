ab81d7a2573de216048902f4983b9221f95e0603
Gary Brown <gary@brownuk.com>
2017-02-27 12:12:56 +0000

    Separate okhttp rules into separate module

--- rules/java-okhttp/src/test/java/io/opentracing/contrib/agent/okhttp/OkHttpITest.java
+++ rules/java-okhttp/src/test/java/io/opentracing/contrib/agent/okhttp/OkHttpITest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.opentracing.contrib.agent.integration;
+package io.opentracing.contrib.agent.okhttp;
 
 import static org.junit.Assert.assertEquals;
 
